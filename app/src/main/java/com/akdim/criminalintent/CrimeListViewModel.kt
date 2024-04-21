cb8954ef6837c01c70d62c077a2a40dcba18be12
Redouan <redouan.akdim@web.de>
2024-04-21 21:46:41 +0200

    build: change build.gradle

--- app/src/main/java/com/akdim/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/akdim/criminalintent/CrimeListViewModel.kt
@@ -1,12 +1,26 @@
 package com.akdim.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
+private const val TAG = "CrimeListViewModel"
 class CrimeListViewModel : ViewModel() {
     private val crimeRepository = CrimeRepository.get()
     val crimes = mutableListOf<Crime>()
+
+    init{
+        Log.d(TAG, "init starting")
+        viewModelScope.launch{
+            Log.d(TAG, "coroutine launched")
+
+            crimes += loadCrimes()
+            Log.d(TAG, "Loading crimes finished")
+        }
+    }
     suspend fun loadCrimes(): List<Crime> {
         return crimeRepository.getCrimes()
     }
